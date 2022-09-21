feb4908978f80ac64c38f39b2929052e50007159
anna <a.sahradyan72@gmail.com>
2022-09-21 16:04:31 +0400

    create authRouter

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const mongoose = require("mongoose");
 const config = require("config");
 const app = express();
 const PORT = config.get("serverPort");
-
+const authRouter = require("./routes/auth.routes");
+app.use(express.json());
+app.use("/api/auth", authRouter);
 const start = async () => {
     try {
-       await mongoose.connect(config.get("dbUrl"))
+        await mongoose.connect(config.get("dbUrl"))
         app.listen(PORT, () => {
-            console.log("Server started on port")
+            console.log(`Server started on port http://localhost:${PORT}`)
         })
     } catch (err) {
 
