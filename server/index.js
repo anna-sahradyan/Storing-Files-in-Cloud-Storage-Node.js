4b21ac78f9ae997d0e80e2659e31db92b575c664
anna <a.sahradyan72@gmail.com>
2022-09-22 00:48:11 +0400

    create login

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 const app = express();
+const cors = require("cors");
 const PORT = config.get("serverPort");
 const authRouter = require("./routes/auth.routes");
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 const start = async () => {
